448517aab0f558582603507e99f1b8ed0dddb8a7
Dominik Schidlowski <dominik@schidlowski.eu>
2024-10-23 00:04:42 +0200

    fix nft ruleset garbage

--- roles/nftables/files/reload_domains.py
+++ roles/nftables/files/reload_domains.py
@@ -3,6 +3,7 @@
 import socket
 import yaml
 import os
+import typing
 
 # config = {
 #     "ACME_SH_IPS": [
@@ -31,13 +32,17 @@ def load_ips(domains, version):
             pass
 
     if len(ipHostnameTuple) == 0:
+        # add localhost if no ip found because nf_tables does not allow empty sets
         if version == 4:
             ipHostnameTuple.add(("127.0.0.1", "localhost"))
         else:
             ipHostnameTuple.add(("::1", "localhost"))
     return ipHostnameTuple
 
-def serialize_nftables_define(name, ipHostnameTuple):
+def serialize_nftables_define(name, ipHostnameTuple: typing.Set[typing.Tuple[str, str]]):
+    # order ipHostnameTuple by ip
+    ipHostnameTuple = sorted(ipHostnameTuple, key=lambda x: x[0])
+
     nftables_define = f"define {name} = " + "{\n"
     ipLines = []
     first = True
@@ -59,13 +64,52 @@ def load_config(file):
     with open(file) as f:
         return yaml.safe_load(f)
     
-def serialize_nftable(config):
-    nftables = []
-    for key, value in config.items():
-        nftables.append(serialize_nftables_define(key+"_V4", load_ips(value, 4)))
-        nftables.append(serialize_nftables_define(key+"_V6", load_ips(value, 6)))
-    return "\n".join(nftables)    
 
+class NftablesDefines:
+    ipv4: typing.Dict[str, typing.Set[typing.Tuple[str, str]]]
+    ipv6: typing.Dict[str, typing.Set[typing.Tuple[str, str]]]
+
+    def __init__(self):
+        self.ipv4 = dict()
+        self.ipv6 = dict()
+
+    def add_domains(self, key, domains: typing.Set[str]):
+        self.ipv4[key] = load_ips(domains, 4)
+        self.ipv6[key] = load_ips(domains, 6)
+
+    def __eq__(self, value):
+        return self.ipv4 == value.ipv4 and self.ipv6 == value.ipv6
+    
+    def __ne__(self, value):
+        return not self.__eq__(value)
+    
+    def serialize(self):
+        keys = set(self.ipv4.keys()).union(set(self.ipv6.keys()))
+        # sort keys
+        keys = sorted(keys)
+
+        defines = []
+        for key in keys:
+            if key not in self.ipv4:
+                continue
+            value = self.ipv4[key]
+
+            defines.append(serialize_nftables_define(key+"_V4", value))
+
+        for key in keys:
+            if key not in self.ipv6:
+                continue
+            value = self.ipv6[key]
+
+            defines.append(serialize_nftables_define(key+"_V6", value))
+
+        return "\n".join(defines)   
+
+def load_defines(config):
+    defines = NftablesDefines()
+    for key, value in config.items():
+        defines.add_domains(key, value)
+    return defines
 
 def check_nftables():
     return os.system("nft -f /etc/nftables.conf --check") == 0
@@ -78,7 +122,8 @@ def main():
     configFile = os.getenv("CONFIG_FILE", "/etc/reload_domains.yaml")
     outputFile = os.getenv("OUTPUT_FILE", "/etc/nftables.d/reload_domains.nft")
     config = load_config(configFile)
-    serialized = serialize_nftable(config)
+    defines = load_defines(config)
+    serialized = defines.serialize()
 
     if os.path.exists(outputFile):
         with open(outputFile, "r") as f:
